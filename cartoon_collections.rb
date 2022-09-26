57b516eedbf4b55d914e25bcf424f7d2e16fb894
Raúl Reymundi <raul@kickstarter.com>
2022-09-26 14:05:26 -0400

    commit tree d3aabbf0dfde6e8bd3fd8533620ff3ba5a37b8d3

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,20 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each.with_index(1) do |name, i|
+    puts "#{[i]}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.map { |data| data.capitalize + '!' }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? { |data| data.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find do |data|
+    cheese_types.include?(data)
+end
 end
